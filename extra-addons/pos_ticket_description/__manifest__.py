2c78c30b05101b99e70336d648e07e8288dc18e4
Ruben Atila <shinseiki.nge@gmail.com>
2024-09-24 21:07:06 -0400

    Agregando modulo descripcion para ventas tipo servicio

---
+++ extra-addons/pos_ticket_description/__manifest__.py
@@ -0,0 +1,17 @@
+{
+    'name': 'POS Ticket Description Sale',
+    'version': '13.0.1.0.0',
+    'category': 'Point of Sale',
+    'summary': 'Add product sale description to POS ticket',
+    'author': 'Antonio Galli',
+    'website': 'https://www.tk-peru.com',
+    'license': 'LGPL-3',
+    'depends': ['l10n_pe_pos_ticket'],
+    'data': [
+    ],
+    'qweb': [
+        'static/src/xml/pos.xml'
+    ],
+    'installable': True,
+    'auto_install': False,
+}
